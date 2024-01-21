91fa6e799d79a401ef2860a7afa653a81d01469e
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-01-21 10:26:48 -0500

    Implementa funcion para agregar mas JobExperience components
    La estado solo guarda en estos momentos la informacion de los
    fields normales, todavia queda implementar los dateFields y
    textAreaFields

--- src/components/JobExperience.jsx
+++ src/components/JobExperience.jsx
@@ -1,22 +1,107 @@
+import { useState } from "react";
 import Field from "./Field";
 import DateField from "./DateField";
+import Button from "./Button";
 import TextAreaField from "./TextAreaField";
-function JobField({ status }) {
+function JobField({ status, dataObj = null, jobData, handleDataMapUpdate }) {
   return (
     <div className="JobField">
-      <Field type={"text"} status={status} name={"companyName"}></Field>
-      <Field type={"text"} status={status} name={"jobTitle"}></Field>
-      <TextAreaField status={status}></TextAreaField>
-      <DateField status={status} name={"fecha inicio"}></DateField>
-      <DateField status={status} name={"fecha final"}></DateField>
+      <Field
+        type={"text"}
+        label={"Compañia"}
+        status={status}
+        name={"company"}
+        dataObj={dataObj}
+        dataMap={jobData}
+        dataObjProperty={"company"}
+        handleDataMapUpdate={handleDataMapUpdate}
+      ></Field>
+      <Field
+        type={"text"}
+        label={"Posicion"}
+        status={status}
+        name={"jobTitle"}
+        dataObj={dataObj}
+        dataMap={jobData}
+        dataObjProperty={"jobTitle"}
+        handleDataMapUpdate={handleDataMapUpdate}
+      ></Field>
+      <TextAreaField
+        status={status}
+        dataObj={dataObj}
+        dataMap={jobData}
+        dataObjProperty={"description"}
+        handleDataMapUpdate={handleDataMapUpdate}
+      ></TextAreaField>
+      <DateField
+        status={status}
+        name={"startDate"}
+        dataObj={dataObj}
+        dataMap={jobData}
+        dataObjProperty={"startDate"}
+        handleDataMapUpdate={handleDataMapUpdate}
+      ></DateField>
+      <DateField
+        status={status}
+        name={"endDate"}
+        dataObj={dataObj}
+        dataMap={jobData}
+        dataObjProperty={"endDate"}
+        handleDataMapUpdate={handleDataMapUpdate}
+      ></DateField>
     </div>
   );
 }
 
 function JobExperience({ status }) {
+  const initialMap = new Map();
+  initialMap.set("0", {
+    id: "0",
+    company: "",
+    jobTitle: "",
+    description: "",
+    startDate: "",
+    endDate: "",
+  });
+  const [jobData, setJobData] = useState(initialMap);
+
+  function getJobArray() {
+    const array = Array.from(jobData, ([name, value]) => value);
+    return array;
+  }
+  function addJobField() {
+    const newDataObj = {
+      id: `${jobData.size}`,
+      company: "",
+      jobTitle: "",
+      description: "",
+      startDate: "",
+      endDate: "",
+    };
+    const newMap = new Map([...jobData]);
+    newMap.set(newDataObj.id, newDataObj);
+    setJobData(newMap);
+  }
+  const jobDataArray = getJobArray();
   return (
     <div>
-      <JobField status={status}></JobField>
+      {jobDataArray.map((data) => (
+        <JobField
+          status={status}
+          dataObj={data}
+          key={data.id}
+          jobData={jobData}
+          handleDataMapUpdate={setJobData}
+        ></JobField>
+      ))}
+      {status === "edit" && (
+        <Button
+          content={"Añadir otro estudio"}
+          btnClass={"addEducation"}
+          handleClick={addJobField}
+          type={"button"}
+        ></Button>
+      )}
     </div>
   );
 }
