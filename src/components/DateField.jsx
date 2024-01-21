bf1f308ed73727000ca275c914fc6d68e6aec8bf
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-01-20 19:10:24 -0500

    Implementa funcionalidad de guardado de valor de fechas
    Tambien implementa el display de estos valores

--- src/components/DateField.jsx
+++ src/components/DateField.jsx
@@ -1,26 +1,49 @@
-function DateInput({ name }) {
+import { useState } from "react";
+
+function formatDateToString(dateValue) {
+  return dateValue ? dateValue.toISOString().split("T")[0] : "";
+}
+
+function DateInput({ name, dateValue, onInput }) {
+  const dateString = formatDateToString(dateValue);
+  function setDateObj(dateString) {
+    const dateObj = new Date(dateString);
+    onInput(dateObj);
+  }
+
   return (
     <label>
       {name}
-      <input type="date" name={name} />
+      <input
+        type="date"
+        value={dateString}
+        onChange={(e) => setDateObj(e.target.value)}
+        name={name}
+      />
     </label>
   );
 }
 
-function DateInfo({ name }) {
+function DateInfo({ name, dateValue }) {
+  const dateString = formatDateToString(dateValue);
   return (
     <div className="dateInfo">
       <p>{name}</p>
-      <p>Fecha</p>
+      <p>{dateString}</p>
     </div>
   );
 }
 
 function DateField({ status, name }) {
+  const [dateValue, setDateValue] = useState("");
   return status === "edit" ? (
-    <DateInput name={name}></DateInput>
+    <DateInput
+      name={name}
+      dateValue={dateValue}
+      onInput={setDateValue}
+    ></DateInput>
   ) : (
-    <DateInfo name={name}></DateInfo>
+    <DateInfo name={name} dateValue={dateValue}></DateInfo>
   );
 }
 
