ebf9e97df995f2c192a2b3c661a8edae67e4998a
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-01-20 19:09:04 -0500

    Agrega variable de status a componente

--- src/components/InputComponents.jsx
+++ src/components/InputComponents.jsx
@@ -3,9 +3,9 @@ import Field from "./Field";
 function PersonalInfo({ status }) {
   return (
     <div className="inputContainer">
-      <Field type="text" name="nombre" status="edit"></Field>
-      <Field type="email" name="correo" status="edit"></Field>
-      <Field type="tel" name="celular" status="edit"></Field>
+      <Field type="text" name="nombre" status={status}></Field>
+      <Field type="email" name="correo" status={status}></Field>
+      <Field type="tel" name="celular" status={status}></Field>
     </div>
   );
 }
