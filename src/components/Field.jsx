aa56da2a1afa50681cb2f72c2ce02555fa0674be
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-01-22 19:33:05 -0500

    Implementa logica para verificacion de inputs
    Se utilizan las variables de state de cvForm, la funcion pasada como prop
    que validara el value y un state que se encarga de renderizar el mensaje
    de error

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
-function Input({ type, inputName, fieldValue, onInput, label }) {
+function Input({ type, inputName, fieldValue, onInput, label, errorValue }) {
   return (
-    <label>
-      {label}
-      <input
-        type={type}
-        value={fieldValue}
-        name={inputName}
-        onChange={(e) => onInput(e.target.value)}
-      />
-    </label>
+    <div>
+      <label>
+        {label}
+        <input
+          type={type}
+          value={fieldValue}
+          name={inputName}
+          onChange={(e) => onInput(e.target.value)}
+        />
+      </label>
+      <p>{errorValue}</p>
+    </div>
   );
 }
 
@@ -23,6 +26,30 @@ function Info({ type, infoName, value }) {
   );
 }
 
+function validateInput(
+  text,
+  inputValidation,
+  errors,
+  setErrors,
+  setErrorValue,
+  dataObj
+) {
+  const errorText = inputValidation(text);
+  const tempErrorsMap = new Map([...errors]);
+  const errorKey = dataObj ? dataObj.id : name;
+  if (errorText) {
+    setErrorValue(errorText);
+    tempErrorsMap.set(errorKey, errorText);
+    setErrors(tempErrorsMap);
+  } else {
+    setErrorValue("");
+    if (errors.has(errorKey)) {
+      tempErrorsMap.set(errorKey, "");
+      setErrors(tempErrorsMap);
+    }
+  }
+}
+
 function Field({
   type,
   label,
@@ -33,11 +60,25 @@ function Field({
   dataMap,
   dataObjProperty,
   handleDataMapUpdate,
+  errors,
+  setErrors,
+  inputValidation,
 }) {
   const [fieldValue, setFieldValue] = useState("");
+  const [errorValue, setErrorValue] = useState("");
 
   function updateInputValue(text) {
     setFieldValue(text);
+
+    validateInput(
+      text,
+      inputValidation,
+      errors,
+      setErrors,
+      setErrorValue,
+      dataObj
+    );
+
     if (dataObj) {
       const newDataObj = { ...dataObj, [dataObjProperty]: text };
       const newMap = new Map([...dataMap]);
@@ -54,6 +95,7 @@ function Field({
       inputName={index > -1 ? `${name}[${index}]` : name}
       value={fieldValue}
       onInput={updateInputValue}
+      errorValue={errorValue}
     ></Input>
   ) : (
     <Info type={type} infoName={name} value={fieldValue}></Info>
