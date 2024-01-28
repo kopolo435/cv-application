babe30b0bce2a9202af01c773224ec415c837741
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-01-28 11:23:57 -0500

    Mueve submitt button fuera de form
    Agrega contenedor que devuelve cvForm, modifica reglas de estilo

--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -7,6 +7,14 @@ import { useEffect } from "react";
 import Button from "./Button";
 import "../../styles/inputs.css";
 
+function SubmittButton({ handleClick, btnClass, content, type, form }) {
+  return (
+    <button type={type} className={btnClass} onClick={handleClick} form={form}>
+      {content}
+    </button>
+  );
+}
+
 function CvForm({
   setStatus,
   educationData,
@@ -47,53 +55,59 @@ function CvForm({
   }, [submitting, setErrors, setStatus]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <legend>Informacion Personal</legend>
-        <PersonalInfo
-          status={status}
-          personalData={personalData}
-          setPersonalData={setPersonalData}
-          errors={errors}
-          setErrors={setErrors}
-          submitting={submitting}
-        ></PersonalInfo>
-      </fieldset>
-      <fieldset>
-        <legend>Estudios realizados</legend>
-        <EducationInfo
-          status={status}
-          educationData={educationData}
-          setEducationData={setEducationData}
-          errors={errors}
-          setErrors={setErrors}
-          submitting={submitting}
-        ></EducationInfo>
-      </fieldset>
-      <fieldset>
-        <legend>Experiencia laboral</legend>
-        <JobExperience
-          status={status}
-          jobData={jobData}
-          setJobData={setJobData}
-          errors={errors}
-          setErrors={setErrors}
-          submitting={submitting}
-        ></JobExperience>
-      </fieldset>
-      <fieldset>
-        <legend>Habilidades</legend>
-        <SkillInfo
-          status={status}
-          skillData={skillData}
-          setSkillData={setSkillData}
-          errors={errors}
-          setErrors={setErrors}
-          submitting={submitting}
-        ></SkillInfo>
-      </fieldset>
-      <Button type={"submitt"} content={"Guardar contenido"}></Button>
-    </form>
+    <div className="formContainer">
+      <form onSubmit={handleSubmit} id="cvForm">
+        <fieldset>
+          <legend>Informacion Personal</legend>
+          <PersonalInfo
+            status={status}
+            personalData={personalData}
+            setPersonalData={setPersonalData}
+            errors={errors}
+            setErrors={setErrors}
+            submitting={submitting}
+          ></PersonalInfo>
+        </fieldset>
+        <fieldset>
+          <legend>Estudios realizados</legend>
+          <EducationInfo
+            status={status}
+            educationData={educationData}
+            setEducationData={setEducationData}
+            errors={errors}
+            setErrors={setErrors}
+            submitting={submitting}
+          ></EducationInfo>
+        </fieldset>
+        <fieldset>
+          <legend>Experiencia laboral</legend>
+          <JobExperience
+            status={status}
+            jobData={jobData}
+            setJobData={setJobData}
+            errors={errors}
+            setErrors={setErrors}
+            submitting={submitting}
+          ></JobExperience>
+        </fieldset>
+        <fieldset>
+          <legend>Habilidades</legend>
+          <SkillInfo
+            status={status}
+            skillData={skillData}
+            setSkillData={setSkillData}
+            errors={errors}
+            setErrors={setErrors}
+            submitting={submitting}
+          ></SkillInfo>
+        </fieldset>
+      </form>
+      <SubmittButton
+        type={"submitt"}
+        content={"Guardar contenido"}
+        form={"cvForm"}
+      ></SubmittButton>
+    </div>
   );
 }
 
